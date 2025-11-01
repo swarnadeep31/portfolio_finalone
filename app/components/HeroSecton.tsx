680948aa78aceb8084b93964b6440ffbdec9d1cd
Swarnadeeproy31 <swarnadeeproy57@email.com>
2025-11-01 16:00:33 +0530

    scroll

--- app/components/HeroSecton.tsx
+++ app/components/HeroSecton.tsx
@@ -40,7 +40,6 @@ export const HeroSection = () => {
 
       {/* Scroll indicator */}
       <div className="absolute bottom-8 left-1/2 -translate-x-1/2 flex flex-col items-center animate-bounce">
-        <span className="text-sm text-muted-foreground mb-2">Scroll Down</span>
         <ArrowDown className="h-5 w-5 text-primary" />
       </div>
     </section>
